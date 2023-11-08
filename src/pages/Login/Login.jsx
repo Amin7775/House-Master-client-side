3b39b6b1304ae6c134c5a5e2046141dc8790c043
Amin7775 <amin.dpi24@gmail.com>
2023-11-08 20:10:57 +0600

    Completed Login page Design

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,76 @@
+import { Link } from "react-router-dom";
+import LoginBanner from "./LoginBanner/LoginBanner";
 
 const Login = () => {
-    return (
-        <div>
-            This is login
+
+    const handleLogin = e => {
+        e.preventDefault()
+        const form = e.target;
+
+        const email = form.email.value;
+        const password = form.password.value;
+
+        console.log(email,password)
+    }
+
+  return (
+    <div className="min-h-screen bg-[#fbfbfa]">
+      <LoginBanner></LoginBanner>
+      {/* form */}
+      <div className=" py-10 px-5 max-w-2xl mx-auto">
+        <h2 className="text-3xl font-semibold text-gray-700 capitalize dark:text-white text-center ">
+          Login
+        </h2>
+        <form onSubmit={handleLogin}>
+          <div className="grid grid-cols-1 gap-6 mt-4 ">
+            <div className="w-full">
+              <label className="text-gray-700 dark:text-gray-200">Email</label>
+              <input
+                required
+                name="email"
+                type="text"
+                className="block w-full px-4 py-3 mt-2 text-gray-700 border-2 bg-white rounded-sm    focus:border-yellow-300  focus:outline-none focus:bg-inherit "
+              />
+            </div>
+            <div className="w-full">
+              <label className="text-gray-700 dark:text-gray-200">
+                Password
+              </label>
+              <input
+                required
+                name="password"
+                type="text"
+                className="block w-full px-4 py-3 mt-2 text-gray-700 border-2 bg-white rounded-sm  focus:border-yellow-300  focus:outline-none "
+              />
+            </div>
+          </div>
+
+          <div className="flex justify-center mt-8">
+            <button
+              className="px-10 py-3 leading-5 transition-colors duration-200 transform bg-yellow-300 text-black font-medium  hover:bg-yellow-400 focus:outline-none rounded-sm"
+              type="submit"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+        <div className="mt-6 space-y-2">
+        <p className="text-center mt-2">
+                  Login With Google ?{" "}
+                  <span className="text-blue-600 cursor-pointer font-medium">
+                    Click Here
+                  </span>
+                </p>
+                <p className="text-center">
+                  Dont Have An Account ?{" "}
+                  <span className="text-blue-600 font-medium">
+                    <Link to={"/register"}>Click Here To Register</Link>
+                  </span>
+                </p>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/routes/route.jsx
+++ src/routes/route.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layout/MainLayout";
 import Home from "../pages/Home/Home";
+import Login from "../pages/Login/Login";
 
 
 const router = createBrowserRouter([
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
             {
                 path:'/',
                 element: <Home></Home>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
             }
         ])
       },
