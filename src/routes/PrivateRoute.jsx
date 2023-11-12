250c9876dfef051b0504fdad4cfc001bd297cd97
Amin7775 <amin.dpi24@gmail.com>
2023-11-12 21:09:45 +0600

    Used Lottie React And changed Private Route Loading Spinner

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,20 +1,25 @@
-import { useContext } from 'react';
-import { AuthContext } from '../provider/AuthProvider';
-import { Navigate, useLocation } from 'react-router-dom';
+import { useContext } from "react";
+import { AuthContext } from "../provider/AuthProvider";
+import { Navigate, useLocation } from "react-router-dom";
+import Lottie from "lottie-react";
+import spinner from "../assets/LottieFiles/LoadingSpinner.json";
 
-const PrivateRoute = ({children}) => {
-    const {user,loading} = useContext(AuthContext)
-    const location = useLocation()
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+  const location = useLocation();
 
-    if(loading){
-        return <progress className="progress w-96"></progress>
-    }
-    if(user?.email){
-        return children
-    }
+  if (loading) {
+    return (
+      <div className="min-h-[50vh] container mx-auto flex items-center justify-center bg-[#fbfbfa]">
+        <Lottie className="h-32" animationData={spinner} speed={0.5}></Lottie>
+      </div>
+    );
+  }
+  if (user?.email) {
+    return children;
+  }
 
-
-    return <Navigate state={location.pathname} to="/login" replace></Navigate>
+  return <Navigate state={location.pathname} to="/login" replace></Navigate>;
 };
 
 export default PrivateRoute;
